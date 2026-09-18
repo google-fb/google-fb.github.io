ac1a3afb9b5303d394f714217e6e4f3962f287b9
google-fb <154669670+google-fb@users.noreply.github.com>
2026-09-19 01:49:19 +0800

    Fix 2kc7w22dhw8vg.js stub to load 6 base64-safe parts

--- webgpu-voice-changer/_next/static/chunks/2kc7w22dhw8vg.js
+++ webgpu-voice-changer/_next/static/chunks/2kc7w22dhw8vg.js
@@ -1,9 +1,14 @@
 (function(){
-  var s=document.currentScript, src=s.src, n=4, i=0, acc="";
+  var s=document.currentScript, src=s.src, n=6, i=0, acc="";
   function pad(i){return (i<10?"0":"")+i}
+  function dec(b64){
+    var bin=atob(b64.replace(/\s+/g,"")), arr=new Uint8Array(bin.length);
+    for(var j=0;j<bin.length;j++) arr[j]=bin.charCodeAt(j);
+    return new TextDecoder("utf-8").decode(arr);
+  }
   function next(){
     if(i>=n){var e=document.createElement("script");e.text=acc;s.parentNode.insertBefore(e,s.nextSibling);return}
-    fetch(src+".part"+pad(i++)).then(function(r){if(!r.ok)throw new Error(r.status);return r.text()}).then(function(t){acc+=t;next()});
+    fetch(src+".b64."+pad(i++)).then(function(r){if(!r.ok)throw new Error(r.status);return r.text()}).then(function(t){acc+=dec(t);next()});
   }
   next();
 })();
