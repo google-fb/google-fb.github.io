f8f0f0025dab087d48bb96d1ca8ae8679270aaa4
google-fb <154669670+google-fb@users.noreply.github.com>
2026-09-19 02:56:40 +0800

    Patch 27t_qfc-3_lzs.js stub with fake currentScript.src

--- webgpu-voice-changer/_next/static/chunks/27t_qfc-3_lzs.js
+++ webgpu-voice-changer/_next/static/chunks/27t_qfc-3_lzs.js
@@ -2,7 +2,13 @@
   var s=document.currentScript, src=s.src, n=8, i=0, acc="";
   function pad(i){return (i<10?"0":"")+i}
   function next(){
-    if(i>=n){var e=document.createElement("script");e.text=acc;s.parentNode.insertBefore(e,s.nextSibling);return}
+    if(i>=n){
+      acc=acc.replace(/"object"==typeof document\?document\.currentScript:void 0/g,"({src:"+JSON.stringify(src)+"})");
+      var e=document.createElement("script");
+      e.text=acc;
+      s.parentNode.insertBefore(e,s.nextSibling);
+      return;
+    }
     fetch(src+".part"+pad(i++)).then(function(r){if(!r.ok)throw new Error(r.status);return r.text()}).then(function(t){acc+=t;next()});
   }
   next();
